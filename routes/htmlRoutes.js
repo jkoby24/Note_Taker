b94cb6243b8b540ac8dfd77ea314d8e6720b463f
jkoby24 <63992346+jkoby24@users.noreply.github.com>
2020-08-09 21:28:21 -0400

    Initial commit. Created routes and server.js file

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// path for URL
+const path = require('path');
+
+module.exports = function (app) {
+  // The code below handles when a user visits a page
+  app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // If no matching route is found default to home
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// Creates an express server
+const app = express();
+
+// Sets an initial port.
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// points server to route files
+
+require('./routes/htmlRoutes')(app);
+
+//Starts the server
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
